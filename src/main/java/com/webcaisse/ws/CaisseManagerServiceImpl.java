ab6cd808ccb4be0165bb30e93b690e544d938557
abdessalem <belhadj.abdessalem@gmail.com>
2014-08-17 01:03:12 +0200

    dev ws

--- src/main/java/com/webcaisse/ws/CaisseManagerServiceImpl.java
+++ src/main/java/com/webcaisse/ws/CaisseManagerServiceImpl.java
@@ -2,17 +2,12 @@ package com.webcaisse.ws;
 
 import java.util.List;
 
-import javax.jws.WebMethod;
-import javax.jws.WebService;
-
 import com.webcaisse.ws.pojo.Famille;
 
 
-@WebService
-public class CaisseManagerServiceImpl {
+public class CaisseManagerServiceImpl implements CaisseManagerService{
 
 	
-	@WebMethod
 	public List<Famille> getFamillesActivees() {
 		return null;
 	}
